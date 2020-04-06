f721ade459c30d41b5133489cc47f96e6c100150
bruce willis <fashun_vyctum@yahoo.com>
2020-04-06 21:40:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,12 @@
 class Dog
-  fido = Dog.new
-  fido.name = "Fido"
-end
+  def name=(dogs_name)
+    this_dogs_name = dogs_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
+  
   def bark
     puts "woof!"
   end
